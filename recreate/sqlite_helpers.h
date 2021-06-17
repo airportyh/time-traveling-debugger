27f4c3cea56166eb218bf384067a13e0f8de1dbc
Toby Ho <airportyh@gmail.com>
2021-06-17 17:10:29 +0000

    added support for print output

--- recreate/sqlite_helpers.h
+++ recreate/sqlite_helpers.h
@@ -3,21 +3,21 @@
 // and that "db" is an accessible sqlite3 * variable.
 #define SQLITE(f)                                                          \
     {                                                                      \
-        int i;                                                             \
-        i = sqlite3_ ## f;                                                 \
-        if (i != SQLITE_OK) {                                              \
-            set_error(i, "%s failed with status %d on line %d: %s\n",      \
-                     #f, i, __LINE__, sqlite3_errmsg(db));                 \
+        int _i_;                                                             \
+        _i_ = sqlite3_ ## f;                                                 \
+        if (_i_ != SQLITE_OK) {                                              \
+            set_error(_i_, "%s failed with status %d on line %d: %s\n",      \
+                     #f, _i_, __LINE__, sqlite3_errmsg(db));                 \
             return 1;                                                      \
         }                                                                  \
     }                                                                      \
 
 #define SQLITE_STEP(stmt)                                        \
     {                                                            \
-        int i;                                                   \
-        i = sqlite3_step(stmt);                                  \
-        if (i != SQLITE_DONE) {                                  \
-            set_error(i, "Step failed on line %d - %s", __LINE__, sqlite3_errmsg(db));\
+        int _i_;                                                   \
+        _i_ = sqlite3_step(stmt);                                  \
+        if (_i_ != SQLITE_DONE) {                                  \
+            set_error(_i_, "Step failed on line %d - %s", __LINE__, sqlite3_errmsg(db));\
             return 1;                                            \
         }                                                        \
     }                                                            \
